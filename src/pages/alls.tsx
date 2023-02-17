897a3b55a9bac799dce1c067503ba82458914b3b
Daeun Kim <daeunkim212@gmail.com>
2023-02-17 20:18:00 +0900

    feat: 전체 page

---
+++ src/pages/alls.tsx
@@ -0,0 +1,12 @@
+import Layout from "components/atoms/layout";
+import NavBar from "components/organisms/nav-bar";
+
+export default function Alls() {
+  return (
+    <>
+      <Layout type="nav">
+        <NavBar url={"/alls"} />
+      </Layout>
+    </>
+  );
+}
