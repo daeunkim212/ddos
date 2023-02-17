cb3cfd6afd45d92eabbfc057a3fa44991ca522f2
Daeun Kim <daeunkim212@gmail.com>
2023-02-17 20:18:20 +0900

    feat: 송금 page

---
+++ src/pages/remits.tsx
@@ -0,0 +1,12 @@
+import Layout from "components/atoms/layout";
+import NavBar from "components/organisms/nav-bar";
+
+export default function Remits() {
+  return (
+    <>
+      <Layout type="nav">
+        <NavBar url={"/remits"} />
+      </Layout>
+    </>
+  );
+}
