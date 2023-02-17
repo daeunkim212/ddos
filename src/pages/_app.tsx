4404e609ef4a6f7539cf3fea72d50db121d4aa91
Daeun Kim <daeunkim212@gmail.com>
2023-02-17 20:15:21 +0900

    feat: apply style global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import NavBar from "components/organisms/nav-bar";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-screen h-screen">
+      <Component {...pageProps} />
+    </div>
+  );
 }
