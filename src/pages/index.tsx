1b6c92ef467b875dfa4841ca134b27c7b040db08
Daeun Kim <daeunkim212@gmail.com>
2023-02-17 20:17:06 +0900

    feat: created 홈 페이지

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import Header from "components/atoms/header";
+import Layout from "components/atoms/layout";
+import Item from "components/molecules/item";
+import NavBar from "components/organisms/nav-bar";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      
+      <Layout type="nav">
+        <NavBar url={"/"} />
+      </Layout>
     </>
-  )
+  );
 }
